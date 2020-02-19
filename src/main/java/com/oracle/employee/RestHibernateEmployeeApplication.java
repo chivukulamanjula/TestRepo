ee5bd11728d740e914d71e993c8c6820444b3265
Manjula_chivukula <manjula.chivukula@gmail.com>
2020-02-18 23:19:30 -0700

    First Commit

---
+++ src/main/java/com/oracle/employee/RestHibernateEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestHibernateEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestHibernateEmployeeApplication.class, args);
+	}
+
+}
